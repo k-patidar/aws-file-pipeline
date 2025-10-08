31bc27141a607baf3fdacc279741f13fcb72e0b8
kundan Patidar <kunalpatidar4803@gmail.com>
2025-10-08 23:51:12 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "File test  ci cd change!"
+    return " Deshbord for File test  ci cd change !"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5003)
